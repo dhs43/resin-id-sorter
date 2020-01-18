f51503eb8f4dc5a8e87b8bd9f2c340dcfa1ec714
chud <chad.murph.johnson@gmail.com>
2020-01-18 03:13:14 -0800

    connected firebase

--- app/src/main/java/com/example/resin_id_sorter/MainActivity.java
+++ app/src/main/java/com/example/resin_id_sorter/MainActivity.java
@@ -2,18 +2,47 @@ package com.example.resin_id_sorter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
 
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users").child("chad").child("2");
+        myRef.setValue("111");
+
+//        // Read from the database
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d("myTag", value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w("myTag", "Failed to read value.", error.toException());
+//            }
+//        });
 
         Button btn1 = (Button)findViewById(R.id.button_scan);
         Button btn2 = (Button)findViewById(R.id.button_stats);
@@ -32,6 +61,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
 }
