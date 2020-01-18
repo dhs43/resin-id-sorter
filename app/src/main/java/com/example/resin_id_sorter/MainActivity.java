35c0d6e955e68a00100adc181488def483739066
chud <chad.murph.johnson@gmail.com>
2020-01-17 23:59:27 -0800

    Created skeleton for UI

--- app/src/main/java/com/example/resin_id_sorter/MainActivity.java
+++ app/src/main/java/com/example/resin_id_sorter/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.resin_id_sorter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button)findViewById(R.id.button_scan);
+        Button btn2 = (Button)findViewById(R.id.button_stats);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ScanActivity.class));
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StatsActivity.class));
+            }
+        });
     }
+
+
 }
